1004fd78f226329adf661ddd69b0f86579770831
Mikael.Tadesse <Mikael.Tadesse@miningelement.com>
2024-11-24 18:47:15 +0200

    дополнил .env.* новой переменной для указания base в конфиге Vite в зависимости от мода сборки

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,20 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 8080
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  plugins: [vue()]
+export default defineConfig(({ command, mode }) => {
+  const base = mode === 'development' ? '/admin' : ''
+
+  return {
+    base,
+    server: {
+      port: 8080
+    },
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    plugins: [vue()]
+  }
 })
